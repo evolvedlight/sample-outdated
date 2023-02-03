121e15aa3cebb40c221bd6eef943241732ce93a3
Stephen Brown <steve@brown.bg>
2023-02-03 09:57:28 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+Dictionary<string, int> points = new()
+{
+    { "James", 9001 },
+    { "Jo", 3474 },
+    { "Jess", 11926 }
+};
+
+string json = JsonConvert.SerializeObject(points, Formatting.Indented);
+
+Console.WriteLine(json);
